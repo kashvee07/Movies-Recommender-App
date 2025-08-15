d55a4afb63a42b40de3925d5757c8f6d2b791d46
Kashvee Singh <151728117+kashvee07@users.noreply.github.com>
2025-08-15 23:26:14 +0530

    Update movies.py

--- movies.py
+++ movies.py
@@ -1,29 +1,31 @@
 import streamlit as st
 import pickle
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 # Load movie data
 movies = pickle.load(open('movies.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+# Recalculate similarity at runtime
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(movies['tags']).toarray()
+similarity = cosine_similarity(vectors)
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = similarity[index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    recommended_movies = [movies.iloc[i[0]].title for i in movie_list]
+    return recommended_movies
 
 st.title("🎬 Movie Recommendation System")
-
-selected_movie = st.selectbox(
-    "Choose a movie:",
-    movies['title'].values
-)
+selected_movie = st.selectbox("Choose a movie:", movies['title'].values)
 
 if st.button("Recommend"):
-    def recommend(movie):
-        index = movies[movies['title'] == movie].index[0]
-        distances = similarity[index]
-        movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-        recommended_movies = []
-        for i in movie_list:
-            recommended_movies.append(movies.iloc[i[0]].title)
-        return recommended_movies
-
     recommendations = recommend(selected_movie)
     st.write("### Recommended Movies:")
     for m in recommendations:
         st.write(m)
+
+
